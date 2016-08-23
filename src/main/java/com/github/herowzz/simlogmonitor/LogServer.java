e78acaa0cf6e9fe0b2c195cb4e9a4645e9323e96
herowzz <herowzz@qq.com>
2016-08-23 11:09:56 +0800

    main start

--- src/main/java/com/github/herowzz/simlogmonitor/LogServer.java
+++ src/main/java/com/github/herowzz/simlogmonitor/LogServer.java
@@ -1,9 +1,19 @@
 package com.github.herowzz.simlogmonitor;
 
+import com.github.herowzz.simlogmonitor.collect.ColletSocketServer;
+import com.github.herowzz.simlogmonitor.watcher.WatcherServer;
+
 public class LogServer {
 
-	public static void main(String[] args) {
-		
+	private static int watcherPort = 8088;
+	private static int collectPort = 9988;
+
+	public static void main(String[] args) throws Exception {
+		ColletSocketServer colletServer = new ColletSocketServer(collectPort);
+		colletServer.start();
+
+		WatcherServer watcherServer = new WatcherServer(watcherPort);
+		watcherServer.start();
 	}
 
 }
